01fe30e46d7ef19312d1235483cf77e49f7a6cc2
Jay Sardhara <jay.fablead@gmail.com>
2024-08-01 12:55:22 +0530

    fix deliver charges

--- lib/ui/ordersScreen/OrdersScreen.dart
+++ lib/ui/ordersScreen/OrdersScreen.dart
@@ -269,7 +269,7 @@ class _OrdersScreenState extends State<OrdersScreen> {
               padding: const EdgeInsets.symmetric(vertical: 5),
               child: Center(
                 child: Text(
-                  'Payment Method :'.tr() + ' ${orderModel.paymentMethod=="cod"?"Cash on delivery":orderModel.paymentMethod.toString()}',
+                  'Payment Method :'.tr() + ' ${orderModel.paymentMethod=="cod"?"Cash on delivery":"Online"}',
                   style: TextStyle(
                       color: Color(COLOR_PRIMARY), fontWeight: FontWeight.bold),
                 ),
